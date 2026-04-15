f14cd0730749e3653c1049a9c35a8ec64a3afa5c
Filipe Lima <filipe.slima2@ufpe.br>
2026-04-15 19:46:46 -0300

    Update topicsbtn.js

--- assets/js/topicsbtn.js
+++ assets/js/topicsbtn.js
@@ -4,3 +4,22 @@ $(document).ready(function() {
     console.log("CLICK WORKED");
   });
 });
+
+$(document).ready(function() {
+  $(document).on("click", "#topics-toggle", function(e) {
+    e.stopPropagation(); // prevent immediate closing
+    $(".side-nav").toggleClass("active");
+  });
+
+  $(document).on("click", function(e) {
+    const sidebar = $(".side-nav");
+
+    if (
+      sidebar.hasClass("active") &&
+      !$(e.target).closest(".side-nav").length &&
+      !$(e.target).closest("#topics-toggle").length
+    ) {
+      sidebar.removeClass("active");
+    }
+  });
+});
